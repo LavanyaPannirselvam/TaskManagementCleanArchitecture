c71870933cef0627c5f196c21ccf2aa0f41634a8
LavanyaPannirselvam <88311895+LavanyaPannirselvam@users.noreply.github.com>
2023-06-09 18:25:51 +0530

    Add files via upload

---
+++ TaskManagement/Library/Models/Enums/PriorityType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementLibrary.Enums
+{
+    public enum PriorityType
+    {
+        HIGH, MEDIUM, LOW, NONE
+
+    }
+}
---
+++ TaskManagement/Library/Models/Project.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagementLibrary.Enums;
+using SQLite;
+
+namespace TaskManagementLibrary.Models
+{
+    public class Project
+    {
+        private readonly int id = 1;
+        [PrimaryKey]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Desc { get; set; }
+        public string CreatedBy { get; set; }
+        public StatusType Status { get; set; }
+        public PriorityType Priority { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public Project(string name, string desc, StatusType status, PriorityType type, DateTime startDate, DateTime endDate)
+        {
+            Id = id++;
+            Name = name;
+            Desc = desc;
+            Status = status;
+            Priority = type;
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+        public Project() { }
+        
+    }
+}
+//need to implement util for id creation
