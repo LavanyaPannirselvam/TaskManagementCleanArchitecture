c6426bf4d82f3511a966313bb693d23e6a6239f7
LavanyaPannirselvam <88311895+LavanyaPannirselvam@users.noreply.github.com>
2023-06-26 18:36:37 +0530

    Add files via upload

---
+++ TaskManagement/Library/Data/DBManager/GetTasksListDataManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagementLibrary.Data.DBHandler;
+using TaskManagementLibrary.Domain;
+using TaskManagementLibrary.Domain.Usecase;
+
+namespace TaskManagementLibrary.Data.DBManager
+{
+    public class GetTasksListDataManager : TaskManagementDataManager, IGetTasksListDataManager
+    {
+        public GetTasksListDataManager(IDBHandler dbHandler) : base(dbHandler)
+        {
+        }
+
+        public void GetTasks(GetTasksListRequest request, IUsecaseCallbackBasecase<GetTasksListResponse> response)
+        {
+            var list = DbHandler.AssignedTasksList(request.projectId);
+            GetTasksListResponse taskResponse = new GetTasksListResponse();
+            taskResponse.Tasks = list;
+            ZResponse<GetTasksListResponse> zResponse = new ZResponse<GetTasksListResponse>();
+            zResponse.Response = "";
+            zResponse.Data = taskResponse;
+            response.OnResponseSuccess(zResponse);
+        }
+    }
+}
