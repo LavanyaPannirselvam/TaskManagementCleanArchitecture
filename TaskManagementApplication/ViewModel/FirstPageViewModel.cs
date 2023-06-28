eeefb7d9b3a7ecab08f8e064c7d609a98cc108f2
LavanyaPannirselvam <88311895+LavanyaPannirselvam@users.noreply.github.com>
2023-06-28 18:22:53 +0530

    Add files via upload

---
+++ TaskManagementApplication/ViewModel/FirstPageViewModel.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.Toolkit.Collections;
+using NSwag.Collections;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TaskManagementLibrary;
+using TaskManagementLibrary.Domain;
+using TaskManagementLibrary.Domain.Usecase;
+using TaskManagementLibrary.Models;
+
+namespace TaskManagementCleanArchitecture.ViewModel
+{
+    public class FirstPageViewModel : FirstPageViewModelBase
+    {
+        private GetProjectsList _getProjectsList;
+        //private GetUsersList _getUsersList; 
+        
+        public override void GetProjectsList()
+        {
+            _getProjectsList = new GetProjectsList(new PresenterGetProjectsList(this),new GetProjectListRequest(new CancellationTokenSource()));
+            _getProjectsList.Execute();
+        }
+
+        //public override void GetUsersList(int projectId)
+        //{
+        //    _getUsersList = new GetUsersList(new GetUsersListRequest(projectId,new CancellationTokenSource()),new PresenterGetUsersList(this));
+        //    _getUsersList.Execute();
+        //}
+    }
+
+
+    public class PresenterGetProjectsList : IPresenterGetProjectsListCallback
+    {
+        public FirstPageViewModel firstPageViewModel;
+
+        public PresenterGetProjectsList(FirstPageViewModel firstPage)
+        {
+            firstPageViewModel = firstPage;
+        }
+
+        public void OnError(BException errorMessage)
+        {
+        }
+
+        public void OnFailure(ZResponse<GetProjectListResponse> response)
+        {
+        }
+
+        public async void OnSuccessAsync(ZResponse<GetProjectListResponse> response)
+        {
+            await SwitchToMainUIThread.SwitchToMainThread(() =>
+            {
+                PopulateData(response.Data.Projects);
+            });
+        }
+
+        private void PopulateData(List<Project> data)
+        {
+            foreach (var p in data)
+            {
+                firstPageViewModel.ProjectsList.Add(p);
+            }
+        }
+    }
+
+
+    //public class PresenterGetUsersList : IPresenterGetUsersListCallback
+    //{
+    //    private FirstPageViewModel _firstPageViewModel;
+
+    //    public PresenterGetUsersList(FirstPageViewModel firstPageViewModel)
+    //    {
+    //        _firstPageViewModel = firstPageViewModel;
+    //    }
+
+    //    public void OnError(BException errorMessage)
+    //    {
+            
+    //    }
+
+    //    public void OnFailure(ZResponse<GetUsersListResponse> response)
+    //    {
+            
+    //    }
+
+    //    public async void OnSuccessAsync(ZResponse<GetUsersListResponse> response)
+    //    {
+    //        await SwitchToMainUIThread.SwitchToMainThread(() =>
+    //        {
+    //            PopulateData(response.Data.AssignedUserList);
+    //            //_firstPageViewModel.projectWithUsersList.Add();
+    //        });
+    //    }
+
+    //    private void PopulateData(List<User> data)
+    //    {
+    //        //TODO : if no users,msg that no users were assigned yet
+    //        foreach (var p in data)
+    //        {
+    //            _firstPageViewModel.UsersList.Add(p);               
+    //        }
+    //    }
+    //}
+
+
+    public abstract class FirstPageViewModelBase : NotifyPropertyBase
+    {
+        public ObservableCollection<Project> ProjectsList = new ObservableCollection<Project>();
+        //public ObservableCollection<User> UsersList = new ObservableCollection<User>();
+        public ObservableCollection<ProjectWithUsersBO> projectWithUsersList = new ObservableCollection<ProjectWithUsersBO>();
+        public ProjectWithUsersBO projectWithUsersBO = new ProjectWithUsersBO();
+        //public abstract void GetUsersList(int projectId);
+       
+        public abstract void GetProjectsList();
+        
+    }
+
+   
+
+}
+//need to do ProjectWithUsersBO
---
+++ TaskManagementApplication/ViewModel/LoginViewModel.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TaskManagementLibrary;
+using TaskManagementLibrary.Domain;
+using TaskManagementLibrary.Domain.Usecase;
+using TaskManagementLibrary.Models;
+using Windows.UI.Xaml;
+using static TaskManagementLibrary.Models.User;
+
+namespace TaskManagementCleanArchitecture.ViewModel
+{
+    public class LoginViewModel : LoginViewModelBase
+    {
+        Login login;
+        public override void ValidateUser(string userId, string password)
+        {
+            login = new Login(new PresenterLoginCallback(this),new LoginRequest(new CancellationTokenSource(),userId, password));
+            login.Execute();
+        }
+    }
+
+
+    public class PresenterLoginCallback : IPresenterLoginCallback
+    {
+        private LoginViewModel loginViewModel;
+
+        public PresenterLoginCallback(LoginViewModel loginViewModel)
+        {
+            this.loginViewModel = loginViewModel;
+        }
+
+        public async void OnError(BException errorMessage)
+        {
+            await SwitchToMainUIThread.SwitchToMainThread(() =>
+            {
+                loginViewModel.TextBoxVisibility = Visibility.Visible;
+                loginViewModel.LoginResponseValue = errorMessage.exceptionMessage.ToString();
+            });
+        }
+
+        public async void OnFailure(ZResponse<LoginResponse> response)
+        {
+            await SwitchToMainUIThread.SwitchToMainThread(() =>
+            {
+                loginViewModel.TextBoxVisibility = Visibility.Visible;
+                loginViewModel.LoginResponseValue = response.Response.ToString();
+            });
+        }
+
+        public async void OnSuccessAsync(ZResponse<LoginResponse> response)
+        {
+            await SwitchToMainUIThread.SwitchToMainThread(() =>
+            {
+                loginViewModel.CurrentUser = response.Data.currentUser;
+                loginViewModel.LoginView?.UpdateLoginPage(loginViewModel.CurrentUser);
+            });
+        }
+    }
+
+
+    public abstract class LoginViewModelBase : NotifyPropertyBase
+    {
+        private string _response = String.Empty;
+        public string LoginResponseValue
+        {
+            get { return this._response; }
+            set
+            {
+                _response = value;
+                OnPropertyChanged(nameof(LoginResponseValue));
+            }
+        }
+
+        private Visibility _textBoxVisibility = Visibility.Collapsed;
+        public Visibility TextBoxVisibility
+        {
+            get { return _textBoxVisibility; }
+            set
+            {
+                _textBoxVisibility = value;
+                OnPropertyChanged(nameof(TextBoxVisibility));
+
+            }
+        }
+
+        private LoggedInUserBO _currentUser;
+        public LoggedInUserBO CurrentUser
+        {
+            get { return this._currentUser; }
+            set
+            {
+                _currentUser = value;
+                OnPropertyChanged(nameof(CurrentUser));
+            }
+        }
+
+        public abstract void ValidateUser(string userId,string password);
+        public ILoginView LoginView { get; set; }
+    }
+
+
+    public interface ILoginView
+    {
+        void UpdateLoginPage(LoggedInUserBO currentUser);
+    }
+}
+//login view model's call back will have setuser method with current user -> loginViewModel
+//set user will invoke the event -> login.xaml.cs
+//even subscription will happen at main.xaml.cs
+
