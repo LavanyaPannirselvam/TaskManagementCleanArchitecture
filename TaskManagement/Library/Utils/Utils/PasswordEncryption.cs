0920199cffbdd8bb2d98acf2024ebf0c020087a1
LavanyaPannirselvam <88311895+LavanyaPannirselvam@users.noreply.github.com>
2023-07-06 21:47:13 +0530

    Add files via upload

---
+++ TaskManagement/Library/Utils/Utils/PasswordEncryption.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementLibrary.Utils
+{
+    public static class PasswordEncryption
+    {
+        public static byte[] EncryptPassword(string str)
+        {
+            SHA256 sha256 = SHA256Managed.Create();
+            byte[] hashValue;
+            UTF8Encoding objUtf8 = new UTF8Encoding();
+            hashValue = sha256.ComputeHash(objUtf8.GetBytes(str));
+            return hashValue;
+        }
+
+        public static string BytesToString(byte[] bytes)
+        {
+            using (MemoryStream stream = new MemoryStream(bytes))
+            {
+                using (StreamReader streamReader = new StreamReader(stream))
+                {
+                    return streamReader.ReadToEnd();
+                }
+            }
+        }
+    }
+}
