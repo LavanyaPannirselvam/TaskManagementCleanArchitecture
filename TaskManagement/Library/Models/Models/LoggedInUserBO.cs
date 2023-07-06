ad6b23c8fe9f501d69f7633ed950cb978e9875df
LavanyaPannirselvam <88311895+LavanyaPannirselvam@users.noreply.github.com>
2023-07-06 21:46:11 +0530

    Add files via upload

---
+++ TaskManagement/Library/Models/Models/LoggedInUserBO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementLibrary.Models
+{
+    public class LoggedInUserBO
+    {
+        public User LoggedInUser { get; set; }
+        public string WelcomeText { get; set; }
+        public LoggedInUserBO()
+        { }
+    }
+}
---
+++ TaskManagement/Library/Models/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagementLibrary.Enums;
+using SQLite;
+
+namespace TaskManagementLibrary.Models
+{
+    public class User
+    {
+        private readonly int id=1;
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        [PrimaryKey]
+        public string Email { get; set; }
+        public Role Role { get; set; }
+        public User(string name, string email, Role role)
+        {
+            UserId = id++;
+            Name = name;
+            Email = email;
+            Role = role;
+        }
+        public User() { }
+        
+    }
+}
