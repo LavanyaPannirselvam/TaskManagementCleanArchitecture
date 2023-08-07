e8fd4e56abf5abc29f43764b3cd901c1370dc5b7
LavanyaPannirselvam <88311895+LavanyaPannirselvam@users.noreply.github.com>
2023-08-07 18:34:21 +0530

    Add files via upload

---
+++ TaskManagementApplication/View/UserControl/ListViewItems.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using TaskManagementLibrary.Models;
+using TaskManagementLibrary.Notifications;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace TaskManagementCleanArchitecture.View.UserControls
+{
+    public sealed partial class ListViewItems : UserControl
+    {
+        public ListViewItems()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Grid_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            //button.Visibility = Visibility.Visible;
+            DeleteUserButton.Visibility = Visibility.Visible;
+        }
+
+        private void Grid_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            //button.Visibility = Visibility.Collapsed;
+            DeleteUserButton.Visibility = Visibility.Collapsed;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            var data = ((FrameworkElement)sender).DataContext as UserBO;
+            UIUpdation.OnUserSelectedRemove(data);
+        }
+    }
+}
